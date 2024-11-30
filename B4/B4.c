8955e1b0c34a309361ba69ca19946f45688abeb9
Duyha2000 <duytd19405c@st.uel.edu.vn>
2024-12-01 00:16:44 +0700

    project mid term

--- B4/B4.c
+++ B4/B4.c
@@ -1,61 +1,20 @@
 #include <stdio.h>
-
 #include <stdio.h>
-// /Users/mac0s/Documents/LearnCWithHung
-// /Users/mac0s/Documents/LearnCWithHung/B1/B1.c
-//int main (int argc, char * argv []){
-//    FILE* fp;
-//    char ch;
-//
-//    if(argc != 2) printf ("an argumenent is missing");
-//    else {
-//        if ((fp = fopen (argv [1], "r"))= NULL){
-//            printf ("error open thr file\n");
-//            exit (1);
-//        }
-//        do {
-//            ch = fgetc (fp);
-//            if (ch != EOF) putchar (ch);
-//        }
-//        while (ch != EOF);
-//        fclose(fp);
-//    }
-//}
+
 
 int main(void) {
-    int arr[5];
-    int a;
-    for (int i = 0; i < 5; i++) {
-        int num;
-        scanf("%d", &num);
-        arr[i] = num;
-    }
-    printf("nhap a: ");
-    scanf("%d", &a);
-    int c, d;
+//  1 [3] 5 [8] 9 -> tăng dần
 
-    for (int i = 0; i < 5; i++) {
-        for (int j = i + 1; j < 5; j++) {
-            if (arr[i] + arr[j] == a) {
-                c = arr[i];
-                d = arr[j];
-            }
-        }
-    }
-    printf("2 so %d %d", c, d);
+//  (3 - 1 ) * ( 3 - 5) < 0
+//  ( 8 - 5 ) * ) * ( 8 - 9) < 0
+
+// 9 [7] 5 2 1
+//  ( 7 - 9) * (7 - 5) < -
+
+
+// 1 3 2 4 9
+//  (3 - 1 ) * ( 3 - 2) > 0
+
     return 0;
 }
 
-// Input:
-// Output:
-
-
-
-
-// Nhũng bài toán lieên quan vị trí khởi tạo biến pos = -1
-// vòng lặp for luôn có cú pháp for i
-// Input dùng 1 vòng for để nhập,Xử lý logic, in phải dùng 1 vòng for khác
-//    sum = 0 (store cộng dồn) -> sum += ...
-//    count = 0 (Store đếm) -> count++
-//    multiply = 1 ->   sum *= ...
-// khi muốn in 1 lần thì in ngoài vòng for/while
--- IOFIle/main.c
+++ IOFIle/main.c
@@ -1,18 +1,119 @@
 #include <stdio.h>
 
 int main() {
-    FILE *f;
-//    f = fopen ("ten_file", "mode"); // r/o/e
-//Mode:
-//r	Mở file để đọc	Nếu file không tồn tại thì hàm fopen() trả về con trỏ NULL
-// w	Mở file để ghi	Nếu file đã tồn tại thì sẽ làm việc với file đó, nếu file chưa tồn tại sẽ tạo 1 file mới
-// a	Mở file text lên để ghi tiếp vào cuối file mà không xóa nội dung cũ trong file	Nếu file đã tồn tại thì sẽ làm việc với file đó, nếu file chưa tồn tại sẽ tạo 1 file mới
-//    f = fopen("/Users/mac0s/Documents/LearnCWithHung/IOFIle/input.txt", "r");
 
-    f = fopen("/Users/mac0s/Documents/LearnCWithHung/IOFIle/output.txt", "w");
-    char s[100] = "28tech.com.vn";
-//    Ghi vào file:  fprintf(f, ...);
-    fprintf(f, "%d %.2lf %.c %s\n", 100, 3.14, '@', s);
-    fclose(f);
+
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        scanf("%d", &num);
+        arr[i] = num;
+    }
+
+    int check = 1; // 0 : false - 1 true
+    // -> 1 2 3 4 [5] 3
+
+    // ( 5 - 3 ) * ( 3 - 1) > 0
+
+    // ( 3 - 5 ) * ( 5 - 3 ) < 0
+
+    // [1 9]
+    // [9 1]
+    // size: > 2 -> 3 5 9 -> ( 9 - 5 ) * ( 5 - 3 )
+    // size: 4 -> 3 5 8 9 ->
+    for (int i = 2; i < n; i++) {
+        if ((arr[i] - arr[i - 1]) * (arr[i] - arr[i + 1]) < 0) {
+            check = 0;
+        }
+
+    }
+    if (check == 0) printf("no");
+    else printf("yes");
+
     return 0;
 }
+
+
+
+//  1 [3] 5 [8] 9 -> tăng dần
+
+//  (3 - 1 ) * ( 3 - 5) < 0
+//  ( 8 - 5 ) * ) * ( 8 - 9) < 0
+
+// 9 [7] 5 2 1
+//  ( 7 - 9) * (7 - 5) < 0
+
+// 1 3 2 4 9
+//  (3 - 1 ) * ( 3 - 2) > 0
+//    FILE *f1, *f2;
+//    f1 = fopen("/Users/mac0s/Documents/LearnCWithHung/IOFIle/input.txt", "r");
+//    f2 = fopen("/Users/mac0s/Documents/LearnCWithHung/IOFIle/output.txt", "w");
+//    int n; // khởi tạo giá trị trong vùng nhớ
+//    // - - - - (n) - - - - -
+//    fscanf(f1, "%d", &n);
+//
+//    int arr[n];
+//
+//    for (int i = 0; i < n; i++) {
+//        fscanf(f1, "%d", &arr[i]);
+//    }
+//
+//    int tar;
+//
+//    fscanf(f1, "%d", &tar);
+//    // '3' 5 2 9
+//    // target 7
+//
+//    // 2 vong for long nhau
+//    // 5 2
+//    int a, b;
+//    for (int i = 0; i < n; i++) {
+//        for (int j = i + 1; j < n; j++) {
+//            if (arr[i] + arr[j] == tar) {
+//                a = arr[i];
+//                b = arr[j];
+//                break;
+//            }
+//        }
+//    }
+//    fprintf(f2, "%d %d", a, b);
+//    return 0;
+//}
+
+
+//int main() {
+//    int n; // khởi tạo giá trị trong vùng nhớ
+//    // - - - - (n) - - - - -
+//    scanf("%d", &n);
+//    int arr[n];
+//
+//    for (int i = 0; i < n; i++) {
+//        int num;
+//        scanf("%d", &num);
+//        arr[i] = num;
+//    }
+//
+//    int tar;
+//    printf("nhap target: ");
+//    scanf("%d", &tar);
+//    // '3' 5 2 9
+//    // target 7
+//
+//    // 2 vong for long nhau
+//    // 5 2
+//    int a, b;
+//    for (int i = 0; i < n; i++) {
+//        for (int j = i + 1; j < n; j++) {
+//            if (arr[i] + arr[j] == tar) {
+//                a = arr[i];
+//                b = arr[j];
+//                break;
+//            }
+//        }
+//    }
+//    printf("%d %d", a, b);
+//    return 0;
+//}
